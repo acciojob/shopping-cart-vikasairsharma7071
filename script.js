d995db54cfbcfef21f4b5e65b042bdbb70c0c722
vikasairsharma7071 <57059631+vikasairsharma7071@users.noreply.github.com>
2023-01-12 07:06:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your code here
+let itemContent = document.getElementById("item-name-input");
+let priceContent = document.getElementById("item-price-input");
+
+let table = document.getElementById("table");
+
+let btn = document.getElementById("add");
+
+let sumVal = document.getElementById("total");
+
+let sum = 0;
+// let sumArr = [];
+
+function addItem(){
+    let item = itemContent.value;
+    let price = priceContent.value;
+
+    
+    if(item != '' && price > 0){
+        // sumArr.push(price);
+        let row = document.createElement("tr");
+        row.innerHTML = `<td>${item}</td><td>${price}</td>`;
+        table.append(row);
+
+        // for(let i=0;i<sumArr.length;i++){
+        //     sum += (Number)(sumArr[i]);
+        // }
+
+        sum = sum + Number(price);
+        sumVal.innerText = "";
+        sumVal.append(sum);
+
+        itemContent.value = '';
+        priceContent.value = '';
+        
+    }
+}
+
+
+
+btn.addEventListener("click",addItem);
